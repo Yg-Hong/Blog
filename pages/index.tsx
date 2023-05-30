41380c615edc6d9d88a35b2f68224370a90f47a9
Aiden <lion0077v@gmail.com>
2023-05-30 12:47:11 +0900

    making init page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import homeStyles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Yungi Hong</title>
+        <title>Aiden Hong</title>
       </Head>
+      <section className={homeStyles.headingMd}>
+        <p>[Aiden Introduction]</p>
+        <p>(This is a website)</p>
+      </section>
+      <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+        <h2 className={homeStyles.headingLg}>Blog</h2>
+        <ul className={homeStyles.list}>
+          <li> 블로그 포스트</li>
+        </ul>
+      </section>
     </div>
   );
 };
